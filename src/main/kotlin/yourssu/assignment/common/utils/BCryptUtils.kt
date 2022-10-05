470095e2f2694c47cf23f1f67ade95c98ae1e2b6
chanwoo <chocw0402@gmail.com>
2022-10-06 01:46:08 +0900

    feat: bcryt utils

---
+++ src/main/kotlin/yourssu/assignment/common/utils/BCryptUtils.kt
@@ -0,0 +1,19 @@
+package yourssu.assignment.common.utils
+
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
+
+@Component
+class BCryptUtils(
+    private val passwordEncoder: PasswordEncoder
+) {
+
+    fun encodePassword(password: String): String {
+        return passwordEncoder.encode(password)
+    }
+
+    fun isMatchedPassword(reqPwd: String, userPwd: String): Boolean {
+        return passwordEncoder.matches(reqPwd, userPwd)
+    }
+
+}
