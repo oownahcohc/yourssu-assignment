2e5d2c434a92b3d50741d6f47c9a8834966c2b06
chanwoo <chocw0402@gmail.com>
2022-10-07 01:13:16 +0900

    feat: article service test 개발 미완

---
+++ src/test/kotlin/yourssu/assignment/app/article/ArticleServiceTest.kt
@@ -0,0 +1,19 @@
+package yourssu.assignment.app.article
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import yourssu.assignment.app.article.service.ArticleService
+import yourssu.assignment.domain.article.repository.ArticleRepository
+import yourssu.assignment.domain.user.User
+
+class ArticleServiceTest: BehaviorSpec({
+    val articleRepository = mockk<ArticleRepository>(relaxed = true)
+
+    val articleService: ArticleService = spyk(ArticleService(articleRepository))
+
+    Given("게시글을 등록하는 경우") {
+
+    }
+})
