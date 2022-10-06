00693ec9d6b090b3a0e2408b6e15a8987792f683
chanwoo <chocw0402@gmail.com>
2022-10-06 17:18:27 +0900

    feat: article 생성 dto

---
+++ src/main/kotlin/yourssu/assignment/app/article/dto/request/CreateArticleDto.kt
@@ -0,0 +1,13 @@
+package yourssu.assignment.app.article.dto.request
+
+import yourssu.assignment.domain.article.Article
+import yourssu.assignment.domain.user.User
+
+data class CreateArticleDto(
+    val title: String,
+    val content: String
+) {
+    fun toArticleEntity(user: User): Article {
+        return Article.newInstance(user, title, content)
+    }
+}
