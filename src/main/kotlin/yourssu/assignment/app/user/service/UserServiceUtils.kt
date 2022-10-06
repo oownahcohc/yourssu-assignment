c8284c51f2bc2ab156adf23cb55d9021bc520c59
chanwoo <chocw0402@gmail.com>
2022-10-06 17:13:27 +0900

    feat: user serivce utils 개발

---
+++ src/main/kotlin/yourssu/assignment/app/user/service/UserServiceUtils.kt
@@ -0,0 +1,33 @@
+package yourssu.assignment.app.user.service
+
+import org.jetbrains.annotations.NotNull
+import yourssu.assignment.common.exception.ResponseResult
+import yourssu.assignment.common.exception.custom.ConflictException
+import yourssu.assignment.common.exception.custom.NotFoundException
+import yourssu.assignment.domain.user.User
+import yourssu.assignment.domain.user.repository.UserRepository
+
+class UserServiceUtils {
+
+    companion object {
+
+        fun validateNotExistUser(userRepository: UserRepository, email: String) {
+            if (userRepository.existByEmail(email)) {
+                throw ConflictException(
+                    String.format("이미 존재하는 유저 (%s) 입니다", email),
+                    ResponseResult.CONFLICT_ALREADY_EXIST_USER_EXCEPTION)
+            }
+        }
+
+        @NotNull
+        fun findUserByEmail(userRepository: UserRepository, email: String): User {
+            return userRepository.findByEmail(email)
+                ?: throw NotFoundException(
+                    String.format("존재하지 않는 유저 (%s) 입니다", email),
+                    ResponseResult.CONFLICT_ALREADY_EXIST_USER_EXCEPTION
+                )
+        }
+
+    }
+
+}
