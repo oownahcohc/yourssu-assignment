79fe8fc1d9229171935fdd50956446b6a9761482
chanwoo <chocw0402@gmail.com>
2022-10-06 17:29:54 +0900

    feat: comment 생성 dto

---
+++ src/main/kotlin/yourssu/assignment/app/comment/dto/request/CommentDto.kt
@@ -0,0 +1,13 @@
+package yourssu.assignment.app.comment.dto.request
+
+import yourssu.assignment.domain.article.Article
+import yourssu.assignment.domain.comment.Comment
+import yourssu.assignment.domain.user.User
+
+data class CommentDto(
+    val content: String
+) {
+    fun toCommentEntity(user: User, article: Article): Comment {
+        return Comment.newInstance(user, article, content)
+    }
+}
