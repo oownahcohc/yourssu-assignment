77e8eb4fe572bac7f6899d7d7ef88a8a784cf7b3
chanwoo <chocw0402@gmail.com>
2022-10-06 17:15:53 +0900

    feat: articleService 개발

---
+++ src/main/kotlin/yourssu/assignment/app/article/service/ArticleService.kt
@@ -0,0 +1,42 @@
+package yourssu.assignment.app.article.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import yourssu.assignment.app.article.dto.request.CreateArticleDto
+import yourssu.assignment.app.article.dto.response.ArticleResponse
+import yourssu.assignment.common.exception.custom.NotFoundException
+import yourssu.assignment.domain.article.repository.ArticleRepository
+import yourssu.assignment.domain.comment.repository.CommentRepository
+import yourssu.assignment.domain.user.User
+
+@Service
+class ArticleService(
+    private val articleRepository: ArticleRepository,
+    private val commentRepository: CommentRepository
+) {
+
+    @Transactional
+    fun registerArticle(request: CreateArticleDto, user: User): ArticleResponse {
+        articleRepository.save(request.toArticleEntity(user)).let {
+            user.addArticle(it)
+            return ArticleResponse.of(it, user)
+        }
+    }
+
+    @Transactional
+    fun updateArticle(request: CreateArticleDto, user: User, articleId: Long): ArticleResponse {
+        ArticleServiceUtils.findByArticleId(articleRepository, articleId).let {
+            it.updateTitleAndContent(request.title, request.content)
+            return ArticleResponse.of(it, user)
+        }
+    }
+
+    @Transactional
+    fun deleteArticle(articleId: Long) {
+        with(ArticleServiceUtils.findByArticleId(articleRepository, articleId)) {
+            this.deleteComment()
+            articleRepository.delete(this)
+        }
+    }
+
+}
