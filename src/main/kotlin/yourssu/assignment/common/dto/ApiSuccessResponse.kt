1c04c0fed3e4c473dcf59c0fd36a80290c1ad4ab
chanwoo <chocw0402@gmail.com>
2022-10-08 18:03:50 +0900

    chore: 기본 SUCCESS response 추가

--- src/main/kotlin/yourssu/assignment/common/dto/ApiSuccessResponse.kt
+++ src/main/kotlin/yourssu/assignment/common/dto/ApiSuccessResponse.kt
@@ -4,11 +4,16 @@ import org.springframework.http.ResponseEntity
 import yourssu.assignment.common.exception.ResponseResult
 
 class ApiSuccessResponse {
+
     companion object{
+
+        val SUCCESS = success(ResponseResult.SUCCESS_OK, ResponseResult.SUCCESS_OK.message)
+
         fun <T> success(responseResult: ResponseResult, data: T): ResponseEntity<T> {
             return ResponseEntity
                 .status(responseResult.statusCode.status)
                 .body(data)
         }
+
     }
 }
