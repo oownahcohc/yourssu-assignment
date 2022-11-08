25a1b053de4b6e8bdb5d7581551b409d69d502f9
chanwoo <chocw0402@gmail.com>
2022-11-08 13:16:15 +0900

    feat : jwt 를 통해 발급한 token response dto

--- src/main/kotlin/yourssu/assignment/app/auth/dto/response/TokenResponseDto.kt
+++ src/main/kotlin/yourssu/assignment/app/auth/dto/response/TokenResponseDto.kt
@@ -1,10 +1,9 @@
 package yourssu.assignment.app.auth.dto.response
 
-class TokenResponseDto(
+data class TokenResponseDto(
     val accessToken: String,
     val refreshToken: String
 ) {
-
     companion object {
         fun of(accessToken: String, refreshToken: String): TokenResponseDto {
             return TokenResponseDto(accessToken, refreshToken)
