fafdb7f283b76232ec8534b45d572a7a18d46580
chanwoo <chocw0402@gmail.com>
2022-11-08 13:16:43 +0900

    feat : Login 시 response

---
+++ src/main/kotlin/yourssu/assignment/app/auth/dto/response/LoginResponse.kt
@@ -0,0 +1,18 @@
+package yourssu.assignment.app.auth.dto.response
+
+import yourssu.assignment.common.utils.HttpHeaderUtils
+import yourssu.assignment.domain.user.entity.UserRole
+
+data class LoginResponse(
+    val email: String,
+    val username: String,
+    val role: UserRole,
+    val accessToken: String,
+    val refreshToken: String
+) {
+    companion object {
+        fun of(email: String, username: String, role: UserRole, accessToken: String, refreshToken: String): LoginResponse {
+            return LoginResponse(email, username, role, HttpHeaderUtils.withBearerToken(accessToken), HttpHeaderUtils.withBearerToken(refreshToken))
+        }
+    }
+}
