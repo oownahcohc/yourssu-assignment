7567356131604f249e54e23db64c4b947823922f
chanwoo <chocw0402@gmail.com>
2022-11-08 13:17:21 +0900

    add : login 서비스 개발

--- src/main/kotlin/yourssu/assignment/app/auth/AuthService.kt
+++ src/main/kotlin/yourssu/assignment/app/auth/AuthService.kt
@@ -2,20 +2,24 @@ package yourssu.assignment.app.auth
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import yourssu.assignment.app.auth.dto.request.LoginRequest
 import yourssu.assignment.app.auth.dto.request.ResignRequest
 import yourssu.assignment.app.auth.dto.request.SignupRequest
+import yourssu.assignment.app.auth.dto.response.LoginResponse
 import yourssu.assignment.app.user.service.UserService
 import yourssu.assignment.app.user.service.UserServiceUtils
 import yourssu.assignment.common.exception.custom.UnAuthorizedException
 import yourssu.assignment.common.utils.BCryptUtils
-import yourssu.assignment.domain.user.User
+import yourssu.assignment.common.utils.JwtUtils
+import yourssu.assignment.domain.user.entity.User
 import yourssu.assignment.domain.user.repository.UserRepository
 
 @Service
 class AuthService(
     private val userService: UserService,
     private val userRepository: UserRepository,
-    private val bCryptUtils: BCryptUtils
+    private val bCryptUtils: BCryptUtils,
+    private val jwtUtils: JwtUtils
 ) {
 
     @Transactional
@@ -24,6 +28,14 @@ class AuthService(
         return userService.registerUser(request.toCreateUserDto(bCryptUtils.encodePassword(request.password)))
     }
 
+    @Transactional
+    fun login(request: LoginRequest): LoginResponse {
+        getValidUser(request.email, request.password).let {
+            val tokenResponseDto = jwtUtils.createTokenByUserEmailAndUserRole(it.email, it.role)
+            it.refreshToken = tokenResponseDto.refreshToken
+            return LoginResponse.of(it.email, it.username, it.role, tokenResponseDto.accessToken, tokenResponseDto.refreshToken)
+        }
+    }
 
     @Transactional
     fun resign(request: ResignRequest) {
