9a4192bec274941ae829752126d794daf5f896b7
chanwoo <chocw0402@gmail.com>
2022-11-08 13:10:58 +0900

    add : User Role 필드 추가

--- src/main/kotlin/yourssu/assignment/app/auth/dto/request/SignupRequest.kt
+++ src/main/kotlin/yourssu/assignment/app/auth/dto/request/SignupRequest.kt
@@ -1,8 +1,10 @@
 package yourssu.assignment.app.auth.dto.request
 
 import yourssu.assignment.app.user.dto.request.CreateUserDto
+import yourssu.assignment.domain.user.entity.UserRole
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class SignupRequest(
     @field:NotBlank
@@ -14,9 +16,12 @@ data class SignupRequest(
 
     @field: NotBlank
     val username: String,
+
+    @field: NotNull
+    val role: UserRole
 ) {
     fun toCreateUserDto(encodedPwd: String): CreateUserDto {
-        return CreateUserDto.of(username, email, encodedPwd)
+        return CreateUserDto.of(username, email, encodedPwd, role)
     }
 }
 
--- src/main/kotlin/yourssu/assignment/app/auth/dto/response/SignupResponse.kt
+++ src/main/kotlin/yourssu/assignment/app/auth/dto/response/SignupResponse.kt
@@ -1,12 +1,15 @@
 package yourssu.assignment.app.auth.dto.response
 
+import yourssu.assignment.domain.user.entity.UserRole
+
 data class SignupResponse(
     val email: String,
-    val username: String
+    val username: String,
+    val role: UserRole
 ) {
     companion object{
-        fun of(email: String, username: String): SignupResponse {
-            return SignupResponse(email, username)
+        fun of(email: String, username: String, role: UserRole): SignupResponse {
+            return SignupResponse(email, username, role)
         }
     }
 }
