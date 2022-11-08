d2a102212ac190c31127d8d5b76637d64435a517
chanwoo <chocw0402@gmail.com>
2022-11-08 13:03:48 +0900

    fix : User Entity 컬럼 추가에 따른 test 코드 변경

--- src/test/kotlin/yourssu/assignment/app/auth/AuthServiceTest.kt
+++ src/test/kotlin/yourssu/assignment/app/auth/AuthServiceTest.kt
@@ -11,15 +11,18 @@ import yourssu.assignment.app.user.service.UserService
 import yourssu.assignment.common.exception.custom.ConflictException
 import yourssu.assignment.common.exception.custom.UnAuthorizedException
 import yourssu.assignment.common.utils.BCryptUtils
-import yourssu.assignment.domain.user.User
+import yourssu.assignment.common.utils.JwtUtils
+import yourssu.assignment.domain.user.entity.User
+import yourssu.assignment.domain.user.entity.UserRole
 import yourssu.assignment.domain.user.repository.UserRepository
 
 class AuthServiceTest: BehaviorSpec({
     val userRepository = mockk<UserRepository>(relaxed = true)
     val bCryptUtils = mockk<BCryptUtils>(relaxed = true)
+    val jwtUtils = mockk<JwtUtils>(relaxed = true)
 
     val userService: UserService = spyk(UserService(userRepository))
-    val authService: AuthService = spyk(AuthService(userService, userRepository, bCryptUtils))
+    val authService: AuthService = spyk(AuthService(userService, userRepository, bCryptUtils, jwtUtils))
 
     Given("이미 회원가입한 유저일 경우") {
         every { userRepository.existByEmail(any()) } returns true
@@ -27,7 +30,7 @@ class AuthServiceTest: BehaviorSpec({
         When("해당 이메일과 비밀번호로 회원가입을 시도하면") {
             Then("ConflictException 이 발생한다") {
                 shouldThrow<ConflictException> {
-                    authService.signup(SignupRequest("","",""))
+                    authService.signup(SignupRequest("","","", UserRole.USER))
                 }
             }
         }
@@ -38,10 +41,10 @@ class AuthServiceTest: BehaviorSpec({
 
         When("정상적인 이메일, 비밀번호를 입력하면") {
 
-            val request = SignupRequest("abcd1234@naver.com", "password", "유어슈")
+            val request = SignupRequest("abcd1234@naver.com", "password", "유어슈", UserRole.USER)
             val encoded = bCryptUtils.encodePassword(request.password)
             val createDto = request.toCreateUserDto(encoded)
-            val new = User.newInstance(createDto.username, createDto.email, createDto.password)
+            val new = User.newInstance(createDto.username, createDto.email, createDto.password, UserRole.USER)
 
             every { bCryptUtils.encodePassword(any()) } returns encoded
             every { userRepository.save(any()) } returns new
@@ -64,7 +67,7 @@ class AuthServiceTest: BehaviorSpec({
         val username = "유어슈"
         val password = "PASSWORD"
         val encodePassword = bCryptUtils.encodePassword(password)
-        val user = User.newInstance(username, email, encodePassword)
+        val user = User.newInstance(username, email, encodePassword, UserRole.USER)
 
         every { userRepository.save(any()) } returns user
         every { userRepository.existByEmail(any()) } returns true
