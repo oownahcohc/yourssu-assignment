a6676d99ed29fe17ca4fb17f26ec8a38f97d0a3d
chanwoo <chocw0402@gmail.com>
2022-11-08 13:17:40 +0900

    add : login 컨트롤러 개발

--- src/main/kotlin/yourssu/assignment/app/auth/AuthController.kt
+++ src/main/kotlin/yourssu/assignment/app/auth/AuthController.kt
@@ -4,8 +4,10 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import yourssu.assignment.app.auth.dto.request.LoginRequest
 import yourssu.assignment.app.auth.dto.request.ResignRequest
 import yourssu.assignment.app.auth.dto.request.SignupRequest
+import yourssu.assignment.app.auth.dto.response.LoginResponse
 import yourssu.assignment.app.auth.dto.response.SignupResponse
 import yourssu.assignment.common.dto.ApiSuccessResponse
 import yourssu.assignment.common.exception.ResponseResult.*
@@ -22,7 +24,7 @@ class AuthController(
     @PostMapping("/auth/signup")
     fun signup(@Valid @RequestBody request: SignupRequest): ResponseEntity<SignupResponse> {
         authService.signup(request).let {
-            return ApiSuccessResponse.success(SUCCESS_CREATED_USER, SignupResponse.of(it.email, it.username))
+            return ApiSuccessResponse.success(SUCCESS_CREATED_USER, SignupResponse.of(it.email, it.username, it.role))
         }
     }
 
@@ -34,4 +36,15 @@ class AuthController(
         authService.resign(request)
         return ApiSuccessResponse.SUCCESS
     }
+
+    /**
+     * 회원 로그인
+     */
+    @PostMapping("/auth/login")
+    fun login(@Valid @RequestBody request: LoginRequest): ResponseEntity<LoginResponse> {
+        authService.login(request).let {
+            return ApiSuccessResponse.success(OK_LOGIN, it)
+        }
+    }
+
 }
