309bde7b5d08b525ed20538030df4f656e33a920
chanwoo <chocw0402@gmail.com>
2022-11-08 13:09:40 +0900

    add : User Role, refresh token 필드 추가에 따른 코드 변경

--- src/main/kotlin/yourssu/assignment/app/user/dto/request/CreateUserDto.kt
+++ src/main/kotlin/yourssu/assignment/app/user/dto/request/CreateUserDto.kt
@@ -1,14 +1,17 @@
 package yourssu.assignment.app.user.dto.request
 
+import yourssu.assignment.domain.user.entity.UserRole
+
 data class CreateUserDto(
     val username: String,
     val email: String,
-    val password: String
+    val password: String,
+    val role: UserRole
 ) {
 
     companion object {
-        fun of(username: String, email: String, password: String): CreateUserDto {
-            return CreateUserDto(username, email, password)
+        fun of(username: String, email: String, password: String, role: UserRole): CreateUserDto {
+            return CreateUserDto(username, email, password, role)
         }
     }
 
--- src/main/kotlin/yourssu/assignment/app/user/service/UserService.kt
+++ src/main/kotlin/yourssu/assignment/app/user/service/UserService.kt
@@ -3,7 +3,7 @@ package yourssu.assignment.app.user.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import yourssu.assignment.app.user.dto.request.CreateUserDto
-import yourssu.assignment.domain.user.User
+import yourssu.assignment.domain.user.entity.User
 import yourssu.assignment.domain.user.repository.UserRepository
 
 @Service
@@ -11,7 +11,7 @@ class UserService(private val userRepository: UserRepository) {
 
     @Transactional
     fun registerUser(request: CreateUserDto): User {
-        return userRepository.save(User.newInstance(request.username, request.email, request.password))
+        return userRepository.save(User.newInstance(request.username, request.email, request.password, request.role))
     }
 
 }
