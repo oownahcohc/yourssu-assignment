4c70962155965055c76819a2803d6c9c83f8a3e2
chanwoo <chocw0402@gmail.com>
2022-11-08 12:50:46 +0900

    feat : 인증 어노테이션 개발

---
+++ src/main/kotlin/yourssu/assignment/config/interceptor/Auth.kt
@@ -0,0 +1,8 @@
+package yourssu.assignment.config.interceptor
+
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class Auth
