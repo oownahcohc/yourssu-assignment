1c187bdcfcefe0a811359ef2a7d18ce5095628ec
chanwoo <chocw0402@gmail.com>
2022-11-22 17:41:03 +0900

    add : Admin 용 회원 조회 요청 respository:

--- src/main/kotlin/yourssu/assignment/domain/user/repository/UserRepositoryCustomImpl.kt
+++ src/main/kotlin/yourssu/assignment/domain/user/repository/UserRepositoryCustomImpl.kt
@@ -1,8 +1,13 @@
 package yourssu.assignment.domain.user.repository
 
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.jetbrains.annotations.Nullable
 import yourssu.assignment.domain.user.entity.QUser.*
 import yourssu.assignment.domain.user.entity.User
+import yourssu.assignment.domain.user.entity.UserRole
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class UserRepositoryCustomImpl(private val query: JPAQueryFactory): UserRepositoryCustom {
 
@@ -28,4 +33,67 @@ class UserRepositoryCustomImpl(private val query: JPAQueryFactory): UserReposito
             .fetchOne()
     }
 
+    override fun findUserRoleIsUSERByEmail(email: String): User? {
+        return query
+            .selectFrom(user)
+            .where(
+                user.email.eq(email),
+                user.role.eq(UserRole.USER),
+            )
+            .fetchOne()
+    }
+
+    override fun findAllUser(createdAtStart: String?, createdAtEnd: String?): List<User> {
+        var result: List<User>? = null
+        if (createdAtStart == null && createdAtEnd != null) {
+            result = findAllUserByBefore(createdAtEnd)
+        }
+        else if (createdAtStart != null && createdAtEnd == null) {
+            result = findAllUserByAfter(createdAtStart)
+        }
+        else if (createdAtStart != null && createdAtEnd != null) {
+            result = findAllUserByBetween(createdAtStart, createdAtEnd)
+        }
+        return result!!
+    }
+
+    private fun findAllUserByBetween(createdAtStart: String, createdAtEnd: String): List<User>? {
+        return query
+            .selectFrom(user)
+            .where(
+                user.createdAt.between(
+                    convertStringToLocalDate(createdAtStart).atStartOfDay(),
+                    convertStringToLocalDate(createdAtEnd).atStartOfDay()
+                ),
+                user.role.eq(UserRole.USER)
+            )
+            .orderBy(user.id.desc())
+            .fetch()
+    }
+
+    private fun findAllUserByAfter(createdAt: String): List<User>? {
+        return query
+            .selectFrom(user)
+            .where(
+                user.createdAt.after(convertStringToLocalDate(createdAt).atStartOfDay()),
+                user.role.eq(UserRole.USER)
+            )
+            .orderBy(user.id.desc())
+            .fetch()
+    }
+
+    private fun findAllUserByBefore(createdAt: String): List<User>? {
+        return query
+            .selectFrom(user)
+            .where(
+                user.createdAt.before(convertStringToLocalDate(createdAt).atStartOfDay()),
+                user.role.eq(UserRole.USER)
+            )
+            .orderBy(user.id.desc())
+            .fetch()
+    }
+
+    private fun convertStringToLocalDate(createdAt: String): LocalDate {
+        return LocalDate.parse(createdAt, DateTimeFormatter.ISO_DATE)
+    }
 }
