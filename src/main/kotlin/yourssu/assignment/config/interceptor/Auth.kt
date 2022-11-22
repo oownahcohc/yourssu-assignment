5a5af91909ab2ea908fefbac1e4cee9357c2fc0b
chanwoo <chocw0402@gmail.com>
2022-11-22 17:00:32 +0900

    fix : WebMvcConfigurationSupport 상속 -> WebMvcConfigurer impl 로 변경

--- src/main/kotlin/yourssu/assignment/config/interceptor/Auth.kt
+++
@@ -1,8 +0,0 @@
-package yourssu.assignment.config.interceptor
-
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class Auth
